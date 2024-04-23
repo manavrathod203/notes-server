efed66d9cc4eac4d63c40b7e5cd63466114c153f
manavrathod203 <138814440+manavrathod203@users.noreply.github.com>
2024-04-23 15:40:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 connectDB();
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
